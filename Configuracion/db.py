536fa972c98e39b255f7a6be197f7215e10022ab
klamar10 <51295920+klamar10@users.noreply.github.com>
2023-05-17 19:55:53 -0500

    Update db.py

--- Configuracion/db.py
+++ Configuracion/db.py
@@ -3,9 +3,9 @@ from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 mysql = MySQL()
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_HOST'] = '159.89.48.159'
 app.config['MYSQL_USER'] = 'adrian'
-app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'adminbd_college'
 mysql.init_app(app)
 
