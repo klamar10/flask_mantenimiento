0f690a880c4381abc47b092be98fca7002033b26
Klamar10 <lm_adrian_98@hotmail.com>
2023-05-31 07:12:19 -0500

    Merge branch 'master' into app_qa

--- Configuracion/db.py
+++ Configuracion/db.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 mysql = MySQL()
 
+
 app.config['MYSQL_HOST'] = '159.89.48.159'
 app.config['MYSQL_USER'] = 'adrian'
 app.config['MYSQL_PASSWORD'] = 'password'
